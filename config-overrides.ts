80e21d12beee18a4421212c90ebf7d860484d157
Dương Quý Vinh <Vinh@gmail.com>
2023-03-04 21:07:33 +0700

    update import syntax

---
+++ config-overrides.ts
@@ -0,0 +1,7 @@
+// @ts-ignore
+const { override, useBabelRc } = require('customize-cra');
+
+module.exports = override(
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useBabelRc(),
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import { store } from './store';
+import App from './containers/App/App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import i18n from './app/i18n';
+import i18n from './i18n';
 import { I18nextProvider } from 'react-i18next';
+import { BrowserRouter as Browser } from 'react-router-dom';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </I18nextProvider>
+    <Browser>
+      <I18nextProvider i18n={i18n}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </I18nextProvider>
+    </Browser>
   </React.StrictMode>,
 );
 
