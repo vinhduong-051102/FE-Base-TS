d1d065697690bebcb46167043391de3d0add66d4
Dương Quý Vinh <Vinh@gmail.com>
2023-03-05 11:12:42 +0700

    update not return null for i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -8,6 +8,7 @@ const Languages = ['vi', 'en'];
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
+    returnNull: false,
     // the translations
     // (tip move them in a JSON file and import them,
     // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
