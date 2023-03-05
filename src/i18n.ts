1796ce9b92eac1abddb2df2b3fd586ff38a74f7b
Dương Quý Vinh <Vinh@gmail.com>
2023-03-05 17:23:35 +0700

    update i18n file

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,7 +4,14 @@ import en from './locales/en';
 import vi from './locales/vi';
 import Backend from 'i18next-http-backend';
 
-const Languages = ['vi', 'en'];
+const resources = {
+  en: {
+    translation: en,
+  },
+  vi: {
+    translation: vi,
+  },
+};
 
 i18n
   .use(Backend)
@@ -12,17 +19,12 @@ i18n
   .init({
     debug: true,
     returnNull: false,
-    defaultNS: 'common',
     // the translations
     // (tip move them in a JSON file and import them,
     // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-    resources: {
-      vi,
-      en,
-    },
+    resources,
     lng: 'vi', // if you're using a language detector, do not define the lng option
-    fallbackLng: Languages,
-
+    fallbackLng: 'vi',
     interpolation: {
       escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
     },
