a9a9ebd4a16ff87aa775dc1525df81d61ea76da1
Dương Quý Vinh <Vinh@gmail.com>
2023-03-10 21:54:43 +0700

    update type status code

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -27,7 +27,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='' element={<ErrorPage code={'404'} />} />
+        <Route path='' element={<ErrorPage code={404} />} />
       </Routes>
       {contextHolder}
     </>
--- src/shared/ErrorPage.tsx
+++ src/shared/ErrorPage.tsx
@@ -19,19 +19,19 @@ interface propsType {
 
 const ErrorPage = ({ code }: propsType) => {
   const navigate = useNavigate();
-  const [status, setStatus] = useState<string | number>('500');
+  const [status, setStatus] = useState<string | number>(500);
   const [subTitle, setSubTitle] = useState('Xin lỗi, có lỗi xảy ra.');
 
   useEffect(() => {
     setStatus(code);
     switch (code) {
-      case '403':
+      case 403:
         setSubTitle('Xin lỗi, bạn không được phép truy cập trang này.');
         break;
-      case '404':
+      case 404:
         setSubTitle('Xin lỗi, trang bạn đã truy cập không tồn tại.');
         break;
-      case '500':
+      case 500:
         setSubTitle('Xin lỗi, có lỗi xảy ra.');
         break;
       default:
